04c077652470743d23a5d9f625b6dcec77ff6147
Hugo de Lelis <hugodelelis00@gmail.com>
2024-10-15 11:10:50 -0300

    add select

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import IOption from '../../interface/selectLeng.interface';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  dropdownOpen = false;
 
+options = signal<IOption[]>([
+    { value: 'pt-br', label: 'PT-BR', image: 'assets/icons/brazil.png' },
+    { value: 'en', label: 'EN', image: 'assets/icons/spain.png' },
+    { value: 'esp', label: 'ESP', image: 'assets/icons/USA.png' },
+]);
+
+  selectedOption: any = null;
+
+  toggleDropdown() {
+    this.dropdownOpen = !this.dropdownOpen;
+  }
+
+  selectOption(option: any) {
+    this.selectedOption = option;
+    this.dropdownOpen = false;
+  }
 }
