3270826020ad51afa834bc21b7c8692bb09c7f68
Hugo de Lelis <hugodelelis00@gmail.com>
2024-10-15 15:47:16 -0300

    finish design

--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA ,Component } from '@angular/core';
+import { register } from 'swiper/element/bundle';
+register();
 
 @Component({
   selector: 'app-content',
   standalone: true,
   imports: [],
   templateUrl: './content.component.html',
-  styleUrl: './content.component.scss'
+  styleUrl: './content.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class ContentComponent {
 
